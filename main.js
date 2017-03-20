005f5fadfaba4d9ec0d3fbd833e4f9b3c04c9ddc
John Lee <johnjaehoonlee@gmail.com>
2017-03-20 13:04:53 -0700

    User can view recipe details

--- main.js
+++ main.js
@@ -3,53 +3,87 @@ var recipes = [
     name: 'PECORINO FAVA SALAD RECIPE',
     recipeBy: 'Recipe by Chef Jonathan Boncek in Portland, Oregon',
     description: 'Combine the fava beans in a large bowl with the onions, escarole, and the pecorino (reserve a couple shavings for garnish). Add some red wine vinegar, some olive oil and salt, to taste. Toss to coat the salad with the dressing. Arrange the salad on serving plates and top with the reserved shaved cheese.',
-    recipe: 'http://thelocalpalate.com/recipes/pecorino-fava-salad-recipe/',
-    price: '$1.00'
+    price: '$1.00',
+    servingSize: '4 servings',
+    id: 1
   },
   {
     name: 'CREAMY BROCCOLI SOUP',
     recipeBy: 'Recipe Kecia Johndrow of Tiny Pies in Austin, Texas',
     description: 'Small pieces of broccoli cooked and combined into a cream base makes this soup a filling and enjoyable addition to a lunch of dinner menu. Made with milk, broccoli, flavorings, and spices, Cream of Broccoli Soup can also be used as a base for chicken and rice casseroles.',
-    recipe: 'http://thelocalpalate.com/recipes/tlps-broccoli-soup/',
-    price: '$2.00'
+    price: '$2.00',
+    servingSize: '6 servings',
+    id: 2
   }
 ]
 
-function renderPost(post) {
+function displayItems(post) {
   var $post = document.createElement('div')
   var $name = document.createElement('h4')
   var $recipeBy = document.createElement('h5')
-  var $description = document.createElement('p')
-  var $price = document.createElement('p')
   $name.textContent = post.name
   $recipeBy.textContent = post.recipeBy
-  $description.textContent = post.description
-  $price.textContent = post.price
   $post.appendChild($name)
   $post.appendChild($recipeBy)
-  $post.appendChild($description)
-  $post.appendChild($price)
   return $post
 }
 
 recipes.forEach(function (post) {
-  var $post = renderPost(post)
-  if (post.name === 'PECORINO FAVA SALAD RECIPE') {
-    var $new1 = document.getElementById('new1')
-    $new1.appendChild($post)
+  var $post = displayItems(post)
+  if (post.id === 1) {
+    var $item1 = document.getElementById('item1')
+    $item1.appendChild($post)
   }
-  if (post.name === 'CREAMY BROCCOLI SOUP') {
-    var $new2 = document.getElementById('new2')
-    $new2.appendChild($post)
+  if (post.id === 2) {
+    var $item2 = document.getElementById('item2')
+    $item2.appendChild($post)
   }
 })
 
-var $target1 = document.getElementById('target1')
-$target1.addEventListener('click', function(event) {
-  window.location = 'http://thelocalpalate.com/recipes/tlps-broccoli-soup/'
+var firstButton = document.getElementById('first-button')
+firstButton.addEventListener('click', function(event) {
+  function displayItems(post) {
+    var $post = document.createElement('div')
+    var $detailDescription = document.createElement('p')
+    var $detailPrice = document.createElement('p')
+    var $detailServing = document.createElement('p')
+    $detailDescription.textContent = post.description
+    $detailPrice.textContent = post.price
+    $detailServing.textContent = post.servingSize
+    $post.appendChild($detailDescription)
+    $post.appendChild($detailPrice)
+    $post.appendChild($detailServing)
+    return $post
+  }
+  recipes.forEach(function (post) {
+    var $post = displayItems(post)
+    if (post.id === 1) {
+      var $item1 = document.getElementById('item1')
+      $item1.appendChild($post)
+    }
+  })
 })
 
-var $target2 = document.getElementById('target2')
-$target2.addEventListener('click', function(event) {
-  window.location = 'http://thelocalpalate.com/recipes/tlps-broccoli-soup/'
+var secondButton = document.getElementById('second-button')
+secondButton.addEventListener('click', function(event) {
+  function displayItems(post) {
+    var $post = document.createElement('div')
+    var $detailDescription = document.createElement('p')
+    var $detailPrice = document.createElement('p')
+    var $detailServing = document.createElement('p')
+    $detailDescription.textContent = post.description
+    $detailPrice.textContent = post.price
+    $detailServing.textContent = post.servingSize
+    $post.appendChild($detailDescription)
+    $post.appendChild($detailPrice)
+    $post.appendChild($detailServing)
+    return $post
+  }
+  recipes.forEach(function (post) {
+    var $post = displayItems(post)
+    if (post.id === 2) {
+      var $item2 = document.getElementById('item2')
+      $item2.appendChild($post)
+    }
+  })
 })
